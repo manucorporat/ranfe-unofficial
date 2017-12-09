5e7b772d768bf8e75553c137468d7173881e011c
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2017-12-09 21:42:22 +0100

    improve calendar

--- src/components/calendar-widget/calendar-widget.tsx
+++ src/components/calendar-widget/calendar-widget.tsx
@@ -1,17 +1,10 @@
-import { Component, Event, EventEmitter, Prop, PropWillChange, PropDidChange } from "@stencil/core";
+import { Component, Event, EventEmitter, Prop } from "@stencil/core";
 
 interface Day {
   number: number;
-  xpos: number;
-  ypos: number
+  x: number;
+  y: number
 }
-
-
-const MONTHS = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
-  'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-
-const WEEK_DAYS = ['D', 'L', 'M', 'X', 'J', 'V', 'S'];
-
 
 @Component({
   tag: 'calendar-widget',
@@ -19,118 +12,83 @@ const WEEK_DAYS = ['D', 'L', 'M', 'X', 'J', 'V', 'S'];
 })
 export class CalendarWidget {
 
+  @Prop() locales: string;
+  @Prop() includeLegend = true;
   @Prop({ mutable: true }) year: number;
   @Prop({ mutable: true }) month: number;
-
-  @PropDidChange('month')
-  monthChanged(month: number) {
-    this.fixValues
-  }
+  @Prop({ mutable: true }) day: number;
 
   @Event() calendarSelected: EventEmitter;
 
-  componentDidLoad() {
-    this.fixValues();
-  }
-
-  fixValues() {
+  private getDate() {
     const date = new Date();
-    if (this.year == null) {
-      this.year = date.getFullYear();
-    } else {
+    if (this.year != null) {
       date.setFullYear(this.year);
-      this.year = date.getFullYear();
     }
-    if (this.month == null) {
-      this.month = date.getMonth();
-    } else {
+    if (this.month != null) {
       date.setMonth(this.month);
-      this.month = date.getMonth();
     }
+    date.setDate(1);
+    return date;
   }
 
-  buildCalendar() {
-    this.fixValues();
-    const date = new Date();
-    const month = this.month;
-
-    date.setFullYear(this.year);
-    date.setMonth(month);
-
+  private buildCalendar() {
+    const date = this.getDate();
+    const month = date.getMonth();
     const days: Day[] = [];
-    let day = 1;
     let xpos = 0;
-    let ypos = 0;
-    while(true) {
-      date.setDate(day);
-      if (month !== date.getMonth()) {
-        break;
-      }
+    let ypos = this.includeLegend ? 2 : 1;
+    for (let day = 1; month === date.getMonth(); day++) {
       const x = date.getDay();
       if (x < xpos) {
         ypos++;
       }
       xpos = x;
-      days.push({
-        number: day,
-        xpos: xpos,
-        ypos: ypos
-      });
-      day++;
+      days.push({ number: day, x: xpos + 1, y: ypos });
+      date.setDate(day + 1);
     }
     return days;
   }
 
-  onSelected(day: number) {
+  private onSelected(day: number) {
+    const date = this.getDate();
     this.calendarSelected.emit({
       day: day,
-      month: this.month + 1,
-      year: this.year
+      month: date.getMonth(),
+      year: date.getFullYear()
     });
   }
 
-  onPrev() {
-    this.month = this.month - 1;
+  private renderLegend() {
+    if (!this.includeLegend) {
+      return null;
+    }
+    const date = new Date(Date.UTC(2017, 0, 1));
+    const formater = new Intl.DateTimeFormat(this.locales, {weekday: 'narrow'});
+    const weekDays = [];
+    for (let day = 1; day <= 7; day++) {
+      date.setDate(day);
+      weekDays.push(<div>{formater.format(date)}</div>);
+    }
+    return weekDays;
   }
 
-  onNext() {
-    this.month = this.month + 1;
-  }
-
-  // render() {
-  //   const weekDays = WEEK_DAYS.map(day => {
-  //     return <div>{day}</div>
-  //   });
-  //   const days = this.days.map(day => {
-  //     return <button
-  //     onClick={()=>this.onSelected(day.number)}
-  //       style={{
-  //         gridColumn: day.xpos+1 + '',
-  //         gridRow: day.ypos+2 + '',
-  //       }}>{day.number}</button>;
-  //   });
-  //   return [
-  //     <div class="calendar-title">
-  //       <button class="calendar-button" onClick={() => this.onPrev()}>Prev</button>
-  //       <div>{MONTHS[this.month]} / {this.year}</div>
-  //       <button class="calendar-button" onClick={() => this.onNext()}>Siguiente</button>
-  //     </div>,
-  //     <div class="calendar-container">
-  //       {weekDays}
-  //       {days}
-  //     </div>
-  //   ]
-  // }
-
-  render2() {
-    const days = this.buildCalendar();
-    return days.map(day => (
+  private renderCalendar() {
+    return this.buildCalendar().map(day => (
       <button
+        class={{ 'selected': day.number === this.day }}
         onClick={() => this.onSelected(day.number)}
         style={{
-          gridColumn: `${day.xpos}`,
-          gridRow: `${day.ypos}`,
+          gridColumn: `${day.x}`,
+          gridRow: `${day.y}`,
         }}>{day.number}</button>
     ));
+  }
+
+  render() {
+    return [
+      ...this.renderLegend(),
+      ...this.renderCalendar()
+    ];
   }
 }
