b8137ec522b6272de0dbc5f6a2432da224d9f653
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2020-06-23 21:32:55 +0200

    Update prerendering

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 import {sass} from '@stencil/sass';
 
 export const config: Config = {
+  buildEs5: false,
   bundles: [
     { components: ['admin-page', 'admin-delete', 'admin-new', 'admin-edit'] },
     { components: ['renfe-header', 'renfe-footer'] },
@@ -9,7 +10,22 @@ export const config: Config = {
     { components: ['results-page', 'results-nav', 'results-table', 'weather-container'] },
     { components: ['root-page', 'search-widget'] }
   ],
+  outputTargets: [
+    {
+      type: 'www',
+      serviceWorker: null,
+      baseUrl: 'http://ranfe.vercel.app',
+    }
+  ],
   plugins: [
     sass(),
-  ]
+  ],
+  extras: {
+    cssVarsShim: false,
+    safari10: false,
+    scriptDataOpts: false,
+    initializeNextTick: false,
+    dynamicImportShim: false,
+    shadowDomShim: false,
+  }
 };
